f766d84ef900747fab8ad242f7cfd34d0a1afb83
FatihAkan70 <fatihakan70@gmail.com>
2025-12-18 12:43:13 +0300

    Commit-2
    commit 2 , "Most profitable Commodity ib month(method 1 ) " Added.

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 // Main.java — Students version
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
@@ -9,16 +10,55 @@ public class Main {
     static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
     static String[] months = {"January","February","March","April","May","June",
                               "July","August","September","October","November","December"};
+    static int[][][] profits = new int[MONTHS][DAYS][COMMS];
     
 
     // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
     public static void loadData() {
+        // implementing the data to "profits" multidimensional array
+        for(int m = 0;m < months.length;m++) {
+        try{
+            Scanner sc = new Scanner(Paths.get("Data_Files/" + months[m]+".txt"));
+
+            while(sc.hasNextLine()){
+                String[] words = sc.nextLine().split(",");
+                int day = Integer.parseInt(words[0]); // day - com - profit sırası oldugu ıcın ayırıp okuyorum
+                String commodity = words[1];
+                int c=-1;
+                for(int i=0;i<5;i++){
+                    if(commodities[i].equals(commodity)){
+                        c=i;
+                        break;
+                    }
+                }
+                int profit = Integer.parseInt(words[2]);
+                if(c!=-1){
+                    profits[m][day-1][c] = profit;
+                }
+
+            }
+            sc.close();
+        }catch(Exception e){}
+        }
     }
 
     // ======== 10 REQUIRED METHODS (Students fill these) ========
 
     public static String mostProfitableCommodityInMonth(int month) {
-        return "DUMMY"; 
+        if(month<0 || month >11)return "INVALID_MONTH";
+        int[] profOfComm = new int[5];
+        for(int d = 0;d<28;d++){
+            for(int c = 0;c<5;c++){
+                profOfComm[c] += profits[month][d][c];
+            }
+        }
+        int bestC =0;
+        for(int c =1;c<5;c++){
+            if(profOfComm[c] >profOfComm[bestC]){
+                bestC =c;
+            }
+        }
+        return (commodities[bestC] + " " + profOfComm[bestC]);
     }
 
     public static int totalProfitOnDay(int month, int day) {
